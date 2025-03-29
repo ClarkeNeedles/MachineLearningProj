0305dbd1f6ebdcb9d4658d808c7cd27643056448
cneedles <c.w.needles@gmail.com>
2025-03-29 17:40:23 -0400

    Adding python code

--- 292Project/Data Storage.py
+++ 292Project/Data Storage.py
@@ -1,7 +1,12 @@
 import h5py
 import pandas as pd
 
-df = pd.read_csv()
+data1 = pd.read_csv('/MachineLearningProj/Data/JumpingPhoneInBackPocket.csv')
+data2 = pd.read_csv('/MachineLearningProj/Data/WalkingPhoneInBackPocket.csv')
+data3 = pd.read_csv('/MachineLearningProj/Data/JumpingPhoneInFrontPocket.csv')
+data4 = pd.read_csv('/MachineLearningProj/Data/WalkingPhoneInFrontPocket.csv')
+data5 = pd.read_csv('/MachineLearningProj/Data/JumpingPhoneInHand.csv')
+data6 = pd.read_csv('/MachineLearningProj/Data/WalkingPhoneInHand.csv')
 
 with h5py.File('project_data.h5', 'w') as h5:
 
@@ -23,4 +28,14 @@ with h5py.File('project_data.h5', 'w') as h5:
     # Creating the subgroups under Segmented Data
     segmented_data.create_group('Train')
     segmented_data.create_group('Test')
+
+    # Adding the raw data
+    raw_data['Andrew P'].create_dataset('JumpingPhoneInHand', data=data5.to_numpy())
+    raw_data['Andrew P'].create_dataset('WalkingPhoneInHand', data=data6.to_numpy())
+    raw_data['Ben M'].create_dataset('JumpingPhoneInBackPocket', data=data1.to_numpy())
+    raw_data['Ben M'].create_dataset('WalkingPhoneInBackPocket', data=data2.to_numpy())
+    raw_data['Clarke N'].create_dataset('JumpingPhoneInFrontPocket', data=data3.to_numpy())
+    raw_data['Clarke N'].create_dataset('WalkingPhoneInFrontPocket', data=data4.to_numpy())
+
+
 
